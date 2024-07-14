5b09a73a3a8d0843c1ea257366d09101028e3981
wangzhendong <wangzhendong20@163.com>
2024-07-14 20:48:37 +0800

    init commit

---
+++ src/models/BERT/run_MC.sh
@@ -0,0 +1,11 @@
+export CUDA_VISIBLE_DEVICES=0
+
+python preprocess.py -log_file logs/log -data_name MC
+
+
+python train.py -task abs -mode train -bert_data_path data/MC/bert/ -dec_dropout 0.2  -model_path output/both -sep_optim true -lr_bert 0.002 -lr_dec 0.02 -save_checkpoint_steps 2000 -batch_size 1 -train_steps 12000 -report_every 50 -accum_count 15 -use_bert_emb true -use_interval true -warmup_steps_bert 1000 -warmup_steps_dec 1000 -max_pos 512 -visible_gpus 0  -log_file logs/bert_both_train.log -finetune_bert True -merge=gate -role_weight=0.2 -topic_weight=0.35
+# finetune for fusion_flag
+python train.py -task abs -mode train -bert_data_path data/MC/bert/ -dec_dropout 0.2 -train_from output/both/model_step_xx.pt -model_path output/both_ft -sep_optim true -lr_bert 0.002 -lr_dec 0.02 -save_checkpoint_steps xx -batch_size 1 -train_steps xx -report_every 50 -accum_count 15 -use_bert_emb true -use_interval true -warmup_steps_bert 0 -warmup_steps_dec 0 -max_pos 512 -visible_gpus 0  -log_file logs/bert_both_ft_train.log -finetune_bert True -merge=gate -role_weight=0.5 -topic_weight=0.35 -fusion_flag=True
+python train.py -task abs -mode validate -batch_size 10 -test_batch_size 10 -bert_data_path data/MC/bert/ -log_file logs/bert_both_ft_val.log -model_path output/both_ft -sep_optim true -use_interval true -visible_gpus 0 -max_pos 512 -max_length 200 -alpha 0.95 -min_length 15 -result_path logs/bert_both_ft_val.txt -temp_dir temp/ -test_all=True -merge=gate
+
+python train.py -task abs -mode test -batch_size 1 -test_batch_size 1 -bert_data_path data/MC/bert/ -log_file logs/bert_both_test.log -test_from output/both/model_step_8000.pt -sep_optim true -use_interval true -visible_gpus 0 -max_pos 512 -max_length 200 -alpha 0.95 -min_length 10 -result_path logs/both_ -temp_dir temp/ -merge=gate
